609e5f79a4a1bf6bc8345ac76af6b54d953e3ba6
Andreas Veithen <veithen@apache.org>
2008-11-17 19:35:15 +0000

    Avoid dumping a stacktrace at log level WARN when a service is disabled for a given transport. Indeed this happens whenever a service is deployed that has no configuration for that transport, which is quite frequent.

--- modules/base/src/main/java/org/apache/axis2/transport/base/AbstractTransportListener.java
+++ modules/base/src/main/java/org/apache/axis2/transport/base/AbstractTransportListener.java
@@ -205,7 +205,13 @@ public abstract class AbstractTransportListener implements TransportListener {
                     transportName + " transport: " + ex.getMessage() + ". " + 
                     "This service is being marked as faulty and will not be available over the " +
                     transportName + " transport.";
-            log.warn(msg, ex);
+            // Only log the message at level WARN and log the full stack trace at level DEBUG.
+            // TODO: We should have a way to distinguish a missing configuration
+            //       from an error. This may be addressed when implementing the enhancement
+            //       described in point 3 of http://markmail.org/message/umhenrurlrekk5jh
+            log.warn(msg);
+            log.debug("Disabling service " + serviceName + " for the " + transportName +
+                    "transport", ex);
             BaseUtils.markServiceAsFaulty(serviceName, msg, service.getAxisConfiguration());
             disableTransportForService(service);
             return;
